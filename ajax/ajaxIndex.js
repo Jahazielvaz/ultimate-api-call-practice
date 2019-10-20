422edcc822693165be7b710515a6d4cea6bd064e
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-20 16:37:40 -0600

    Currently having a hard time getting a response using ajax

--- ajax/ajaxIndex.js
+++ ajax/ajaxIndex.js
@@ -1,4 +1,19 @@
 $(() => {
-  
+  let form = $("#form1");
+  let output = $("#user-output");
+  let data = JSON.parse(form.val())
+
+  form.on('submit', (e) => {
+    $.ajax({
+      type: 'POST',
+      data: data,
+      contentType: 'application/json',
+      success: () => {
+        console.log(response.json(data))
+
+      }
+    })
+    e.preventDefault()
+  })
 
 })
--- ajax/app.js
+++ ajax/app.js
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/ajaxIndex.html')
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('Success')
+})
+
 
 //server
 let port = 3000;
