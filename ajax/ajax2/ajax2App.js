00cd18d119c4e354e5f6efb830c1a04ead8cc2a5
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-22 17:05:30 -0600

    Third time rebuilding the connection

--- ajax/ajax2/ajax2App.js
+++ ajax/ajax2/ajax2App.js
@@ -1,49 +1,56 @@
-// Express Setup
+//EXPRESS SETUP
 let express = require('express'),
 app = express(),
 bodyParser = require('body-parser'),
-urlencoded = bodyParser.urlencoded({extended: true})
+urlencoded = bodyParser.urlencoded({extended: true});
 
-app.use('/', express.static(__dirname));
-app.use(urlencoded)
+//Middleware
+app.use(express.static(__dirname));
+app.use(urlencoded);
 
-// DATABASE
-MongoClient = require('mongodb').MongoClient;
+// DATABASE SETUP
+let MongoClient = require('mongodb').MongoClient,
 ObjectId = require('mongodb').ObjectID;
 
-// DB INFO
-let USERNAME = 'imagineTech',
+// DB info
+let DATABASE_NAME = 'start',
+USER_NAME = 'imagineTech',
 PASSWORD = 'Chato225%24%23%40',
-DATABASE_NAME = 'start',
-URL_STRING = `mongodb+srv://${USERNAME}:${PASSWORD}@start-xnmb4.mongodb.net/test?retryWrites=true&w=majority`
+URL_STRING = `mongodb+srv://${USER_NAME}:${PASSWORD}@start-xnmb4.mongodb.net/test?retryWrites=true&w=majority`
 
-let database,
-dbCollection;
-
-// ROUTES
+//Routes
 app.get('/contact', (req, res) => {
-  res.sendFile(__dirname +'/ajax2Index.html')
+  res.sendFile(__dirname + '/ajax2Index.html');
 })
 
 app.post('/contact', (req, res) => {
-  dbCollection.insert(req.body, (err, result) => {
+  collection.insertOne(req.body);
+  res.sendFile(__dirname + '/ajax2Response.html');
+})
+
+
+// DB Variables
+let database,
+collection;
+
+
+//Server
+let port = process.env.port || 4000;
+app.listen(port, (err) => {
+  if(err){
+    console.log(err)
+    throw error
+  }
+
+  MongoClient.connect(URL_STRING, {useNewUrlParser: true}, (err, client) => {
     if(err){
       throw error
     }
 
-    res.sendFile(__dirname + '/ajax2Response.html')
-  })
-})
-
-//Server
-let port = process.env.port || 4000;
-app.listen(port, () => {
-  MongoClient.connect(URL_STRING, {useNewUrlParser: true}, (err, client) => {
-    if(err){
-      throw error;
-    }
     database = client.db(DATABASE_NAME);
-    dbCollection = database.collection('contact')
-
+    collection = database.collection('contact');
   })
+
+
+  console.log(`Listening to port ${port}`);
 })
