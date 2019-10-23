771fe15b1403225e03f318e00814b0fbb4e67369
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-22 19:21:52 -0600

    Iteration 1 of hero object is now complete

--- ajax/ajax3-heroes/ajax3App.js
+++ ajax/ajax3-heroes/ajax3App.js
@@ -19,8 +19,69 @@ PASSWORD = 'Chato225%24%23%40',
 URL_STRING = `mongodb+srv://${USER_NAME}:${PASSWORD}@start-xnmb4.mongodb.net/test?retryWrites=true&w=majority`;
 
 // Hero Object
+{
+  power: '',
+  speed: '',
+  technique: '',
+  intelligence: '',
+  cooperativeness: ''
+}
+
+let heroes = [
+  {
+    name: 'Izuku Midorya',
+    quirk: 'One For All',
+    image: 'https://vignette.wikia.nocookie.net/bokunoheroacademia/images/d/d9/Izuku_Midoriya_school_profile.png/revision/latest?cb=20190129015623',
+    stats: {
+      power: 'E',
+      speed: 'E',
+      technique: 'B',
+      intelligence: 'B',
+      cooperativeness: 'A'
+    }
+  },
+  {
+    name: 'Katsuki Bakugo',
+    quirk: 'Explosion',
+    image: 'https://vignette.wikia.nocookie.net/bokunoheroacademia/images/8/83/Katsuki_Bakugo_School_Uniform_Full_Body.png/revision/latest?cb=20190812065435',
+    stats: {
+      power: 'A',
+      speed: 'B',
+      technique: 'A',
+      intelligence: 'B',
+      cooperativeness: 'E'
+    }
+  },
+  {
+    name: 'Shoto Todoroki',
+    quirk: 'Fire and Ice',
+    image: 'https://vignette.wikia.nocookie.net/bokunoheroacademia/images/c/c4/Shoto_Todoroki_school_profile.png/revision/latest?cb=20170728153443',
+    stats: {
+      power: 'A',
+      speed: 'B',
+      technique: 'A',
+      intelligence: 'B',
+      cooperativeness: 'C'
+    }
+  },
+  {
+    name: 'Tsuyu Asui',
+    quirk: 'Frog',
+    image: 'https://vignette.wikia.nocookie.net/bokunoheroacademia/images/f/ff/Tsuyu_Asui_Full_Body_School_Uniform_Anime.png/revision/latest?cb=20160118114942',
+    stats: {
+      power: 'E',
+      speed: 'C',
+      technique: 'B',
+      intelligence: 'B',
+      cooperativeness: 'A'
+    }
+  },
+]
 
 // Routes
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/ajax3Index.html');
+})
 
 
 // Supporting Variables
@@ -29,10 +90,16 @@ let database, collection;
 
 // Server
 let port = PROCESS.env.port || 5000;
-app.listen(port, (err) => {
-  if(err){
-    throw error;
-  }
+app.listen(port, () => {
+
+  MongoClient.connect(URL_STRING, {useNewUrlParser: true}, (err, client) => {
+    if(err){
+      throw error;
+    }
+
+    database = client.db(DATABASE_NAME);
+    collection = database.collection('heroes');
+  })
 
 
   console.log(`Listening to port ${port}`);
