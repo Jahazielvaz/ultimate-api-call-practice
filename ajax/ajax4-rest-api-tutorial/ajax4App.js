3e131143435b76c0e5e7c309eba748b6ae5ca528
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-29 21:33:44 -0600

    db connected and ready to receive transmission. currently testing to see if the data goes through

--- ajax/ajax4-rest-api-tutorial/ajax4App.js
+++ ajax/ajax4-rest-api-tutorial/ajax4App.js
@@ -8,6 +8,16 @@ urlencoded = bodyParser.urlencoded({extended: false});
 app.use(urlencoded);
 app.use(express.static(__dirname));
 
+// DATABASE
+let MongoClient = require('mongodb').MongoClient;
+let ObjectId = require('mongodb').ObjectID;
+
+// DB Info
+let USERNAME = 'imagineTech',
+PASSWORD = 'Chato225%24%23%40',
+DATABASE_NAME = 'journal',
+URL_STRING = `mongodb+srv://${USERNAME}:${PASSWORD}@start-xnmb4.mongodb.net/test?retryWrites=true&w=majority`;
+
 // Routes
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/ajax4Index.html');
@@ -21,13 +31,26 @@ app.get('/register', (req, res) => {
   res.sendFile(__dirname + '/ajax4Register.html');
 })
 
+app.post('/register', (req, res) => {
+  collection.insert(req.body)
+})
+
+// Variables
+let collection, database;
+
 
 // Server
 let port = process.env.port || 3000;
-app.listen(port, (err) => {
-  if(err){
-    throw error
-  }
+app.listen(port, () => {
+
+  MongoClient.connect(URL_STRING, {useNewUrlParser: true}, (error, client) => {
+    if(error){
+      throw error;
+    }
+    
+    database = client.db(DATABASE_NAME);
+    collection = database.collection('registration');
+  })
 
   console.log(`Listening to port ${port}`);
 })
