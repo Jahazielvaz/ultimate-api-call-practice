95adf04a090bb121093f50df2a7fbd1e1d713309
Jahaziel <jahaziel.vaz@gmail.com>
2019-12-03 20:17:22 -0700

    Repopulating files

--- ajax/summonerswar-characters-api/api/models/charactersModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const characterSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  starLevel: Number
-}, {collection: process.env.COLLECTION})
-
-module.exports = mongoose.model('Characters', characterSchema)
--- ajax/summonerswar-characters-api/app.js
+++ ajax/summonerswar-characters-api/app.js
@@ -1,32 +1,18 @@
-// Dependencies
 const express = require('express');
-const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const morgan = require('morgan');
 
-// General Middleware
-app.use(morgan('dev'));
+// App Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
-// DB
-mongoose.connect(`mongodb+srv://imagineTech:${process.env.PASSWORD}@start-xnmb4.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-// CORS HEADERS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-
-  next()
-})
+app.use(morgan('dev'));
 
 // Routes
-const characterRoutes = require('./api/routes/characters');
-app.use('/characters', characterRoutes);
+
+
+
+
 
 
 module.exports = app;
