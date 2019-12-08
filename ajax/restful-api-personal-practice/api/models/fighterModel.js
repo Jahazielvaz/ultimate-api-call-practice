95b7b7a27453dcd19ba707d5609c1e318ce25145
Jahaziel <jahaziel.vaz@gmail.com>
2019-12-08 00:58:46 -0700

    Individual fighter get request handler is now created

--- ajax/restful-api-personal-practice/api/models/fighterModel.js
+++ ajax/restful-api-personal-practice/api/models/fighterModel.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const fighterSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
-  powerlever: { type: Number, required: true }
+  powerLevel: { type: Number, required: true }
 }, {collection: process.env.COLLECTION});
 
 module.exports = mongoose.model('Fighters', fighterSchema)
--- ajax/restful-api-personal-practice/api/routes/fighterRoutes.js
+++ ajax/restful-api-personal-practice/api/routes/fighterRoutes.js
@@ -9,16 +9,40 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const newFighter = new Fighters({
-    _id: mongoose.Types.ObjectId,
+    _id: mongoose.Types.ObjectId(),
     name: req.body.name,
-    powerLevel: req.body.powerlevel
+    powerLevel: req.body.powerLevel
   })
 
-  res.status(201).json({message: 'Fighter has been posted', newFighter});
+  newFighter.save()
+  .then(data => {
+    console.log(data)
+    res.status(201).json({message: 'Fighter has been posted', data});
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: {
+        message: err
+      }
+    })
+  })
 });
 
 router.get('/:fighterId', (req, res, next) => {
-  res.status(200).json({message: 'Heres your fighter'});
+  const id = req.params.fighterId;
+  Fighters.findById(id)
+  .exec()
+  .then((data) => {
+    res.status(200)
+    .json(data)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500)
+    .json({error:{
+      error: err
+    }})
+  })
 });
 
 router.patch('/:fighterId', (req, res, next) => {
