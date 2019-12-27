ecef82a1df8e24ab4609b80473e975270da53e2d
Jahaziel <jahaziel.vaz@gmail.com>
2019-12-27 10:19:34 -0700

    New setup

--- ajax/restful-api-personal-practice/server.js
+++ ajax/restful-api-personal-practice/server.js
@@ -1,9 +1,8 @@
 const http = require('http');
 const app = require('./app');
-
-const port = process.env.port || 3000;
 const server = http.createServer(app);
 
+const port = process.env.port || 3000;
 server.listen(port, () => {
-  console.log(`Listening to port ${port}`);
-});
+  console.log(`Listening to port ${port}`)
+})
