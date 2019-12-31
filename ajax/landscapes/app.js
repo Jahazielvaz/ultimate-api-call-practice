57c12ad5869520b406fbb5a2249b062c81b6ac00
Jahaziel <jahaziel.vaz@gmail.com>
2019-12-30 19:36:54 -0700

    Post route is now createdand connected to the db. Need to test the connection

--- ajax/landscapes/app.js
+++ ajax/landscapes/app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 // App Middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+// DB
+mongoose.connect(`mongodb+srv://imagineTech:${process.env.PASSWORD}@start-xnmb4.mongodb.net/Landscapes?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // Routes
 const landscapeRoutes = require('./routes/landscapes');
--- ajax/landscapes/routes/landscapes.js
+++ ajax/landscapes/routes/landscapes.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const Place = require('../models/landscapesModel');
 
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'It still works from landscapes routes'
+router.post('/', (req, res, next) => {
+  const place = new Place({
+    _id: mongoose.Types.ObjectId(),
+    name: req.body.name,
+    location: req.body.location,
+    description: req.body.description
+  })
+
+  place.save()
+  .then(result => {
+    res.status(201).json({
+      message: 'Your new spot is been posted',
+      place: result
+    })
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Error',
+      error: err
+    })
   })
 })
 
