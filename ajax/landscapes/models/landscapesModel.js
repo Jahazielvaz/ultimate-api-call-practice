3dbfebe01178a96237d65eecc1fbd8793a02058e
Jahaziel <jahaziel.vaz@gmail.com>
2019-12-30 20:38:50 -0700

    currently have a working example, but still can't understand how that example is different from what I'm doing. Will disect it

--- ajax/landscapes/models/landscapesModel.js
+++ ajax/landscapes/models/landscapesModel.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
 
-const landscapeSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: {type: String, required: true},
-  location: {type: String, required: true},
-  description: {type: String, required: true}
-}, {collection: 'locations'});
+// const landscapeSchema = mongoose.Schema({
+//   _id: mongoose.Schema.Types.ObjectId,
+//   name: {type: String, required: true},
+//   location: {type: String, required: true},
+//   description: {type: String, required: true}
+// }, {collection: 'locations'});
 
-module.exports = mongoose.model('Place', landscapeSchema);
+// module.exports = mongoose.model('Place', landscapeSchema);
+
+const productSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: String,
+  price: Number
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
+
+
+
+
+
+
+
+
+
+
+// 
--- ajax/landscapes/routes/landscapes.js
+++ ajax/landscapes/routes/landscapes.js
@@ -1,29 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Place = require('../models/landscapesModel');
+const Product = require('../models/landscapesModel');
+
+// router.post('/', (req, res, next) => {
+//   const place = new Place({
+//     _id: new mongoose.Types.ObjectId(),
+//     name: req.body.name,
+//   })
+//   place.save()
+//   .then(result => {
+//     res.status(201).json({
+//       message: 'Your new spot is been posted',
+//       place: result
+//     })
+//   })
+//   .catch(err => {
+//     res.status(500).json({
+//       message: 'Error',
+//       error: err
+//     })
+//   })
+// })
 
 router.post('/', (req, res, next) => {
-  const place = new Place({
-    _id: mongoose.Types.ObjectId(),
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    location: req.body.location,
-    description: req.body.description
+    price: req.body.price
+  });
+  product
+  .save()
+  .then(result => {
+    console.log(result)
+  })
+  .catch(err => console.log(err))
+  res.status(201).json({
+    message: "Your product has been posted",
+    createdProduct: product
   })
 
-  place.save()
-  .then(result => {
-    res.status(201).json({
-      message: 'Your new spot is been posted',
-      place: result
-    })
-  })
-  .catch(err => {
-    res.status(500).json({
-      message: 'Error',
-      error: err
-    })
-  })
-})
+}) //End of post route
 
 module.exports = router;
