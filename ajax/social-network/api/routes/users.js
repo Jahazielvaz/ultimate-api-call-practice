2e82621b967aaac5d124ce9afd4482d6d9e168ab
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-19 21:55:25 -0700

    Working on getting the encryption to work

--- ajax/social-network/api/routes/users.js
+++ ajax/social-network/api/routes/users.js
@@ -1,26 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/usersModel');
 const error = require('./error');
 
-router.post('/', (req, res, next) => {
-  const user = new User({
-    _id: mongoose.Types.ObjectId(),
-    username: req.body.username,
-    password: req.body.password
-  });
-
-  user.save()
+router.post('/signup', (req, res, next) => {
+  User.find({username: req.body.username})
+  .exec()
   .then(user => {
-    res.status(201).json({
-      message: 'Your username and password have been successfully stored in our records',
-      user: user
-    });
+    if(user.length >= 1){
+      res.status(422).json({
+        message: "Username already exists"
+      })
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+          res.status(500).json(error)
+        } else {
+          const user = new User({
+            _id: mongoose.Types.ObjectId(),
+            username: req.body.username,
+            password: hash
+          })
 
-    console.log(user);
-  })
+          user.save()
+          .then(result => {
+            res.status(201).json({
+              message: 'Your user id has been successfully stored in our records',
+              user: result
+            })
+          })
+          .catch(error)
+        }
+      })
+    }
+  })//End of then
+
   .catch(error)
 });
 
