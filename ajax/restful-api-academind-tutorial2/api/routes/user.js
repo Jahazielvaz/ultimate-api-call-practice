8dbb48947a68ab6ad8e439e5b565bc72d491d652
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-19 17:17:21 -0700

    Validatiion to check if user exists before creating a record is now available

--- ajax/restful-api-academind-tutorial2/api/routes/user.js
+++ ajax/restful-api-academind-tutorial2/api/routes/user.js
@@ -7,34 +7,42 @@ const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
 
 router.post('/signup', (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if(err){
-      return res.status(500).json({
-        error: err
-      });
+  User.find({username: req.body.username})
+  .exec()
+  .then(user => {
+    if(user.length >= 1){
+      res.status(422).json({
+        message: 'user name exists in our records'
+      })
     } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        username: req.body.username,
-        password: hash
-      });
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+          return res.status(500).json({
+            error: err
+          });
+        } else {
+          const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            username: req.body.username,
+            password: hash
+          });
 
-      user.save()
-      .then(result => {
-        res.status(201).json({
-          message: 'User has been created',
-          user: result
-        });
+          user.save()
+          .then(result => {
+            res.status(201).json({
+              message: 'User has been created',
+              user: result
+            });
 
-        console.log(result)
-      })
-      .catch(err => {
-        res.status(500).json({error: err})
-      })
-    } //end of else block
-  });
-
-
+            console.log(result)
+          })
+          .catch(err => {
+            res.status(500).json({error: err})
+          })
+        }
+      }); //end of bcrypt section
+    } //end of main else block
+  })
 
 })
 
