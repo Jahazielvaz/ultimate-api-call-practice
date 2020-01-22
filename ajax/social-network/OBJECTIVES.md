0af763a823f66fdfc4b2cf5fb58b6b8cc8ed74f7
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-21 17:36:36 -0700

    post route with hashed and salted password created

---
+++ ajax/social-network/OBJECTIVES.md
@@ -0,0 +1,6 @@
+#OBJECTIVES
+- Setup a standard post route, and make a successful Request
+- Make sure the route verifies that the email doesn't exist. If it does, return an error.
+- Make sure the password is hashed
+- Make sure the password is salted
+## ALL ABOVE TASKS COMPLETED
--- ajax/social-network/api/routes/users.js
+++ ajax/social-network/api/routes/users.js
@@ -2,85 +2,45 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
-const User = require("../models/usersModel");
+const User = require('../models/usersModel');
 
-router.get('/', (req, res, next) => {
-  User.find()
-  .exec()
-  .then(result => {
-    res.status(200).json({
-      message: 'Here\'s the full list of users in our database',
-      list: result
-    });
-  })
-  .catch(error => {
-    res.status(500).json({ error: error })
-  })
-
-})//End of get route
-
-router.post('/signin', (req, res, next) => {
-  User.find({username: req.body.username})
-  .exec()
-  .then(user => {
-    if(user.length < 1){
-        return res.status(401).json({
-            message: 'Auth Failed'
-      });
-    }
-
-    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-      if(err){
-          return res.status(401).json({message: 'Auth Failed'})
-      }
-
-      if(result){
-        return res.status(200).json({message: 'Auth Successful'})
-      }
-
-    }) //End of bcrypt compare
-
-
-  }) //End of then block
-  .catch(error => {
-    res.status(500).json({error: error})
-  })
-})
-
-router.post('/register', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
   User.find({username: req.body.username})
   .exec()
   .then(result => {
     if(result.length >= 1){
-      return res.status(422).json({message: 'User already exists'});
+      return res.status(422).json({
+        message: 'Username exists!'
+      });
     } else {
-      bcrypt.hash(req.body.password, 10, (error, hash) => {
-        if(error){
-          return res.status(500).json(error)
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err){
+          res.status(500).json({error: err})
         } else {
           const user = new User({
             _id: mongoose.Types.ObjectId(),
             username: req.body.username,
             password: hash
-          });
+          })
 
           user.save()
           .then(user => {
             res.status(201).json({
-              message: 'Your username already exists',
+              message: 'User has been logged to our records',
               user: user
             })
-          })
-        } //end of inner else statement
-      }) //End of bcrypt hash
+          }) //End of .then
+        }
+      }) //Bcrypt hash
     }
   })
-  .catch(error => {
-    res.status(500).json({error: error})
-  })
-})
+}) //End of post request
+
+
+
+
+
 
 
 module.exports = router;
