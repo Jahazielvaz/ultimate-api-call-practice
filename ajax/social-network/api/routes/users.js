faa2c97f19d478e0646d43331b863e2c9bf81c0a
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-22 21:11:33 -0700

    Currently working on fixing a bug 'secretOrPrivateKey must have a value

--- ajax/social-network/api/routes/users.js
+++ ajax/social-network/api/routes/users.js
@@ -6,6 +6,43 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/usersModel');
 
+router.post('/signin', (req, res, next) => {
+  User.find({email: req.body.email})
+  .exec()
+  .then(user => {
+    if(user.length < 1){
+      return res.status(401).json({message: 'Auth Failed'})
+    }
+
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if(err){
+        res.status(401).json({message: 'Auth Failed'});
+      }
+
+      if(result){
+        const token = jwt.sign(
+          {
+            email: user[0].email,
+            userId: user[0]._id
+          },
+            process.env.JWT_KEY,
+            {
+              expiresIn: '1h'
+            }
+        ); //End of token
+
+        return res.status(200).json({
+          message: 'Auth Successful',
+          token: token
+        }); //End of response
+      } //End of if statement
+    }); //End of bcrypt
+  })
+  .catch(err => {
+    res.status(500).json({error: err})
+  })
+})
+
 router.post('/register', (req, res, next) => {
   User.find({email: req.body.email})
   .exec()
