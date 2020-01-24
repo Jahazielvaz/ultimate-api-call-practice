f3f6523ee590a248c148074bd48677d2240d43a3
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-23 18:47:24 -0700

    jwt is now added to the social network app. Currently working on protecting routes in restful-api-academind-tutorial2

--- ajax/social-network/api/models/usersModel.js
+++ ajax/social-network/api/models/usersModel.js
@@ -9,6 +9,6 @@ const userSchema = mongoose.Schema({
     match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   },
   password: { type: String, required: true }
-}, { collection: 'users' });
+}, {collection: 'users'});
 
 module.exports = mongoose.model('User', userSchema);
--- ajax/social-network/api/routes/users.js
+++ ajax/social-network/api/routes/users.js
@@ -6,56 +6,52 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/usersModel');
 
-router.post('/signin', (req, res, next) => {
+router.post('/login', (req, res, next) => {
   User.find({email: req.body.email})
   .exec()
   .then(user => {
     if(user.length < 1){
-      return res.status(401).json({message: 'Auth Failed'})
+      res.status(422).json({message: 'Auth Failed'});
     }
 
     bcrypt.compare(req.body.password, user[0].password, (err, result) => {
       if(err){
-        res.status(401).json({message: 'Auth Failed'});
+        res.status(422).json({message: 'Auth Failed'});
       }
 
-      if(result){
-        const token = jwt.sign(
-          {
-            email: user[0].email,
-            userId: user[0]._id
-          },
-            process.env.JWT_KEY,
-            {
-              expiresIn: '1h'
-            }
-        ); //End of token
+      const token = jwt.sign(
+        {
+        email: user[0].email,
+        userId: user[0]._id
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: '1h'
+      }
+    ); //End of jwt method.
 
-        return res.status(200).json({
-          message: 'Auth Successful',
-          token: token
-        }); //End of response
-      } //End of if statement
-    }); //End of bcrypt
+    return res.status(200).json({
+      message: 'Auth Success',
+      data: token
+    })
+  }) //End of bcrypt
   })
   .catch(err => {
     res.status(500).json({error: err})
   })
 })
 
-router.post('/register', (req, res, next) => {
+router.post('/registration', (req, res, next) => {
   User.find({email: req.body.email})
   .exec()
   .then(result => {
     if(result.length >= 1){
-      return res.status(401).json({
-        message: 'User exists!'
-      });
+      return res.status(401).json({message: 'User Exists!'});
     }
 
     bcrypt.hash(req.body.password, 10, (err, hash) => {
       if(err){
-        return res.status(500).json({error: err});
+        return res.status(500).json({error: err})
       }
 
       const user = new User({
@@ -65,21 +61,25 @@ router.post('/register', (req, res, next) => {
       });
 
       user.save()
-      .then(user => {
+      .then(data => {
         res.status(201).json({
-          message: 'New user has been registered',
-          user: user
-        })
+          message: 'Your username and password have been registered',
+          user: data
+        });
+
+        console.log(data)
       })
-      .catch(err => {
-        res.status(500).json({error: err})
-      });
-    })
+      .catch(err)
+    }); //End of bcrypt encryption
   })
   .catch(err => {
     res.status(500).json({error: err})
-  }); //end of promise
-}); //end of post request
+  }); //End of promise
+
+
+}); //End of post request
+
+
 
 
 module.exports = router;
