fad826a13a54bcb3ace6d943f479861d38912388
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-25 22:36:24 -0700

    Images added

--- ajax/summonerswar-characters-api/api/models/charactersModel.js
+++ ajax/summonerswar-characters-api/api/models/charactersModel.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const characterSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: String,
-  starLevel: Number
+  name: {type: String, required: true},
+  starLevel: {type: Number, required: true}
 }, {collection: process.env.COLLECTION});
 
 module.exports = mongoose.model('Characters', characterSchema);
