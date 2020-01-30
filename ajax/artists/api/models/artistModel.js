3394476cc8ea18a176f7784692837464941e4614
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-29 23:20:59 -0700

    All Routes successful. Password encryption successful

--- ajax/artists/api/models/artistModel.js
+++ ajax/artists/api/models/artistModel.js
@@ -4,7 +4,8 @@ const artistSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type:String, required: true },
   field: { type: String, required: true },
-  networth: { type: Number, required: true }
+  networth: { type: Number, required: true },
+  password: { type: String, required: true }
 }, { collection: 'artist' });
 
 module.exports = mongoose.model('Artist', artistSchema);
--- ajax/artists/api/routes/artist.js
+++ ajax/artists/api/routes/artist.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Artist = require('../models/artistModel');
+
+router.get('/:artistId', (req, res, next) => {
+  const id = req.params.artistId;
+  Artist.findById(id)
+  .exec()
+  .then(result => {
+    res.status(200).json({message: 'Requested Artist Name', artist: result});
+  })
+  .catch(err => {
+    res.status(500).json({error: err});
+  })
+}); //End of get request
+
 
 router.get('/', (req, res, next) => {
   Artist.find()
@@ -30,28 +44,51 @@ router.post('/', (req, res, next) => {
     }
   })
 
-  const artist = new Artist({
-    _id: mongoose.Types.ObjectId(),
-    name: req.body.name,
-    field: req.body.field,
-    networth: req.body.networth
-  });
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if(err){
+      return res.status(500).json({
+        message: err
+      })
+    }
 
-  artist.save()
+    const artist = new Artist({
+      _id: mongoose.Types.ObjectId(),
+      name: req.body.name,
+      field: req.body.field,
+      networth: req.body.networth,
+      password: hash
+    });
+
+    artist.save()
+    .then(result => {
+      console.log(result.password);
+      res.status(201).json({
+        message: 'New artist has been logged',
+        data: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({error: err})
+    });
+  })
+
+
+}); //End of post route
+
+router.patch('/:artistId', (req, res, next) => {
+  const id = req.params.artistId;
+  Artist.update({_id: id}, {$set: req.body})
+  .exec()
   .then(result => {
-    res.status(201).json({
-      message: 'New artist has been logged',
-      data: result
+    res.status(200).json({
+      message: "Artist has been updated",
+      artist: result
     });
   })
   .catch(err => {
     res.status(500).json({error: err})
-  });
-}); //End of post route
-
-router.patch('/', (req, res, next) => {
-
-})
+  })
+}); //End of patch request
 
 
 module.exports = router;
