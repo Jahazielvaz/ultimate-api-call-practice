67f316f185dcc9380e807bfddc7b256bfb9610af
Jahaziel <jahaziel.vaz@gmail.com>
2020-01-31 12:03:22 -0700

    Both post routes are now cleared, waiting for re-construction

--- ajax/artists/api/routes/artist.js
+++ ajax/artists/api/routes/artist.js
@@ -33,72 +33,7 @@ router.get('/', (req, res, next) => {
   });
 }); //End of get route
 
-router.post('/user', (req, res, next) => {
-  const name = req.body.name;
-  Artist.find({name: name})
-  .exec()
-  .then(result => {
-    if(result.length < 1){
-      return res.status(401).json({message: 'Unauthorized'});
-    }
 
-    bcrypt.compare(req.body.password, result[0].password, (err, artist) => {
-      if(err){
-        return res.status(401).json({message: 'Unathorized'});
-      }
-
-      if(artist){
-        return res.status(200).json({message: "Welcome User", result: artist});
-      }
-    })
-  })
-  .catch(err => {
-    res.status(500).json({error: err})
-  })
-}); //End of post route
-
-router.post('/', (req, res, next) => {
-  const name = req.body.name;
-  Artist.find({name: name})
-  .exec()
-  .then(result => {
-    if(result.length >= 1){
-      return res.status(401).json({
-        message: 'Artist Exists'
-      })
-    }
-  })
-
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if(err){
-      return res.status(500).json({
-        message: err
-      })
-    }
-
-    const artist = new Artist({
-      _id: mongoose.Types.ObjectId(),
-      name: req.body.name,
-      field: req.body.field,
-      networth: req.body.networth,
-      password: hash
-    });
-
-    artist.save()
-    .then(result => {
-      console.log(result.password);
-      res.status(201).json({
-        message: 'New artist has been logged',
-        data: result
-      });
-    })
-    .catch(err => {
-      res.status(500).json({error: err})
-    });
-  })
-
-
-}); //End of post route
 
 router.patch('/:artistId', (req, res, next) => {
   const id = req.params.artistId;
